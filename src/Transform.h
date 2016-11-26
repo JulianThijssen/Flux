f25825b05e1f736102515fa13ef6f2c741d562c4
Julian Thijssen <Nimthora@gmail.com>
2016-11-26 06:22:14 +0100

    Added Transform component

---
+++ src/Transform.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef TRANSFORM_H
+#define TRANSFORM_H
+
+#include "Component.h"
+#include "Vector3f.h"
+
+namespace Flux {
+    class Transform : public Component {
+    public:
+        Transform() :
+            position(0, 0, 0),
+            rotation(0, 0, 0),
+            scale(1, 1, 1)
+        {
+
+        }
+
+        Vector3f position;
+        Vector3f rotation;
+        Vector3f scale;
+    };
+}
+
+#endif /* TRANSFORM_H */
