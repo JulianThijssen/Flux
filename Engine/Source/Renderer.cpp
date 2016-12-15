bb8a770fa74cdcf253e2041a3e14c0976f9edc6a
Julian Thijssen <Nimthora@gmail.com>
2016-12-15 17:49:13 +0100

    Removed unused includes from Renderer

--- Engine/Source/Renderer.cpp
+++ Engine/Source/Renderer.cpp
@@ -2,10 +2,6 @@
 
 #include <glad/glad.h>
 
-#include "ShaderLoader.h"
-#include "ModelLoader.h"
-#include "Path.h"
-
 namespace Flux {
     void Renderer::setClearColor(float r, float g, float b, float a) {
         glClearColor(r, g, b, a);
