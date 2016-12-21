8072a77acfa8f12ecd21a694adf973ec2be8d07d
Julian Thijssen <Nimthora@gmail.com>
2016-12-21 16:40:10 +0100

    Creating a complete texture without mip-maps

--- Engine/Source/TextureLoader.cpp
+++ Engine/Source/TextureLoader.cpp
@@ -32,7 +32,12 @@ namespace Flux {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
+
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+
+        glBindTexture(GL_TEXTURE_2D, 0);
 
         return new Texture(handle);
     }
