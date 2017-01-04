6711347aab882671a32059aa49a2b4f020b8d4b9
Julian Thijssen <Nimthora@gmail.com>
2017-01-04 16:20:31 +0100

    Generating mip-maps for cubemaps

--- Engine/Source/Cubemap.h
+++ Engine/Source/Cubemap.h
@@ -17,8 +17,8 @@ namespace Flux {
             glGenTextures(1, &handle);
             bind();
 
-            glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-            glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+            glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
             glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
             glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
             glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
@@ -28,6 +28,8 @@ namespace Flux {
                 unsigned char* data = stbi_load(paths[i], &width, &height, &bpp, STBI_rgb_alpha);
                 glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
             }
+
+            glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
 
             release();
         }
