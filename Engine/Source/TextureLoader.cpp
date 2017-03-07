3664dda736ddc14ac228eed2b14e8abbd14611d0
Julian Thijssen <Nimthora@gmail.com>
2017-03-07 21:49:24 +0100

    Add utility to load HDR images

--- Engine/Source/TextureLoader.cpp
+++ Engine/Source/TextureLoader.cpp
@@ -23,6 +23,20 @@ namespace Flux {
         return texture;
     }
 
+    Texture* TextureLoader::loadTextureHDR(Path path) {
+        int width, height, bpp;
+
+        float *data = stbi_loadf(path.str().c_str(), &width, &height, &bpp, STBI_rgb_alpha);
+
+        if (!data) {
+            Log::error("Failed to load image at: " + path.str());
+        }
+
+        Texture* texture = createHDR(width, height, data, Sampling::NEAREST);
+
+        return texture;
+    }
+
     Texture* TextureLoader::create(const int width, const int height, const unsigned char* data, Sampling sampling) {
         GLuint handle;
         glGenTextures(1, &handle);
@@ -39,6 +53,30 @@ namespace Flux {
         }
 
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glBindTexture(GL_TEXTURE_2D, 0);
+
+        return new Texture(handle);
+    }
+
+    Texture* TextureLoader::createHDR(const int width, const int height, const float* data, Sampling sampling) {
+        GLuint handle;
+        glGenTextures(1, &handle);
+
+        glBindTexture(GL_TEXTURE_2D, handle);
+
+        if (sampling == Sampling::NEAREST) {
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+        }
+        else {
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        }
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, width, height, 0, GL_RGBA, GL_FLOAT, data);
 
         glGenerateMipmap(GL_TEXTURE_2D);
 
--- Engine/Source/TextureLoader.h
+++ Engine/Source/TextureLoader.h
@@ -10,7 +10,9 @@ namespace Flux {
     class TextureLoader {
     public:
         static Texture* loadTexture(Path path);
+        static Texture* loadTextureHDR(Path path);
         static Texture* create(const int width, const int height, const unsigned char* data, Sampling sampling);
+        static Texture* createHDR(const int width, const int height, const float* data, Sampling sampling);
         static Texture* createEmpty(const int width, const int height, GLint internalFormat, GLenum format, GLenum type, Sampling sampling);
     };
 }
