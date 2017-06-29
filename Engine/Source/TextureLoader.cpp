9ffa1bcf58987976f4130185e0e5e1fb11d468b3
Julian Thijssen <Nimthora@gmail.com>
2017-06-29 18:00:21 +0200

    Add 5x Anisotropic Filtering

--- Engine/Source/TextureLoader.cpp
+++ Engine/Source/TextureLoader.cpp
@@ -104,6 +104,9 @@ namespace Flux {
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         }
+        GLfloat maxAnisotropy;
+        glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &maxAnisotropy);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, maxAnisotropy > 5.0f ? 5.0f : maxAnisotropy);
 
         if (bpp == 1) {
             glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, data);
