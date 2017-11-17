1c48b315f64ee1e608e3ed73271c066bc9befa0e
Julian Thijssen <Nimthora@gmail.com>
2017-11-17 19:15:11 +0100

    Make window non-resizable

--- Engine/Source/Window.cpp
+++ Engine/Source/Window.cpp
@@ -39,6 +39,7 @@ namespace Flux {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
         window = glfwCreateWindow(width, height, title, NULL, NULL);
         glfwMakeContextCurrent(window);
