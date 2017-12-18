fe478762465ea7aefafa3325df847198330204cc
Julian Thijssen <Nimthora@gmail.com>
2017-12-18 17:49:27 +0100

    Add function to create shadow cubemap

--- Engine/Source/Cubemap.h
+++ Engine/Source/Cubemap.h
@@ -72,6 +72,31 @@ namespace Flux {
             release();
         }
 
+        void createShadowMap(const int resolution) {
+            glGenTextures(1, &handle);
+
+            glBindTexture(GL_TEXTURE_CUBE_MAP, handle);
+
+            glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+            glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+            glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+            glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_BORDER);
+            glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE);
+            glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
+
+            // Set the border color to 1 so samples outside of the shadow map have the furthest depth
+            float color[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+            glTexParameterfv(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_BORDER_COLOR, color);
+
+            for (int i = 0; i < 6; i++) {
+                glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_DEPTH_COMPONENT24, resolution, resolution, 0, GL_DEPTH_COMPONENT, GL_FLOAT, 0);
+            }
+
+            glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+        }
+
         void bind() const {
             glBindTexture(GL_TEXTURE_CUBE_MAP, handle);
         }
