fcce66a3b2cd90304c3ccb7f3eae5065b4251015
Julian Thijssen <Nimthora@gmail.com>
2018-03-27 13:46:13 +0200

    Make texture units unsigned ints rather than ints

--- Engine/Source/TextureUnit.h
+++ Engine/Source/TextureUnit.h
@@ -5,34 +5,34 @@
 namespace Flux {
     class TextureUnit {
     public:
-        static const int ALBEDO = 0;
-        static const int NORMAL = 1;
-        static const int ROUGHNESS = 2;
-        static const int METALNESS = 3;
-        static const int STENCIL = 4;
-        static const int EMISSION = 5;
-        static const int IRRADIANCE = 6;
-        static const int PREFILTER = 7;
-        static const int SCALEBIAS = 8;
-        static const int NOISE = 9;
+        static const unsigned int ALBEDO = 0;
+        static const unsigned int NORMAL = 1;
+        static const unsigned int ROUGHNESS = 2;
+        static const unsigned int METALNESS = 3;
+        static const unsigned int STENCIL = 4;
+        static const unsigned int EMISSION = 5;
+        static const unsigned int IRRADIANCE = 6;
+        static const unsigned int PREFILTER = 7;
+        static const unsigned int SCALEBIAS = 8;
+        static const unsigned int NOISE = 9;
 
-        static const int TEXTURE = 0;
-        static const int BLOOM = 1;
+        static const unsigned int TEXTURE = 0;
+        static const unsigned int BLOOM = 1;
 
-        static const int POSITION = 2;
-        static const int DEPTH = 3;
+        static const unsigned int POSITION = 2;
+        static const unsigned int DEPTH = 3;
 
-        static const int SHADOW = 9;
+        static const unsigned int SHADOW = 9;
 
-        static const int TEXTURE0 = 0;
-        static const int TEXTURE1 = 1;
-        static const int TEXTURE2 = 2;
-        static const int TEXTURE3 = 3;
-        static const int TEXTURE4 = 4;
-        static const int TEXTURE5 = 5;
-        static const int TEXTURE6 = 6;
-        static const int TEXTURE7 = 7;
-        static const int TEXTURE8 = 8;
+        static const unsigned int TEXTURE0 = 0;
+        static const unsigned int TEXTURE1 = 1;
+        static const unsigned int TEXTURE2 = 2;
+        static const unsigned int TEXTURE3 = 3;
+        static const unsigned int TEXTURE4 = 4;
+        static const unsigned int TEXTURE5 = 5;
+        static const unsigned int TEXTURE6 = 6;
+        static const unsigned int TEXTURE7 = 7;
+        static const unsigned int TEXTURE8 = 8;
     };
 }
 
